4843bc393f1ada85b70dae5f1b0aa5510c9f3964
Thiago Kufa <thiago.kufa@gmail.com>
2021-02-25 23:52:34 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import ExperienceBar from '../components/ExperienceBar'
-import Profile from '../components/Profile'
 import CompletedChallenges from '../components/CompletedChallenges'
 import Countdown from '../components/Countdown'
 import ChallengeBox from '../components/ChallengeBox'
 import styles from '../styles/pages/Home.module.css'
+import Profile from '../components/Profile'
+
+
 import { CountdownProvider } from '../contexts/CountdownContext'
 
 const Home: React.FC = () => {
